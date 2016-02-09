cd9fd0044ed17fc178e95adbb3bd547b93ea5d54
Kyle Clarke <git@kylehq.com>
2016-02-09 22:23:03 +1300

    Using middleware and httprouter to render pages

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
+	unauthenticatedRouter := NewRouter()
+	unauthenticatedRouter.GET("/", HandleHome)
+	unauthenticatedRouter.GET("/register", HandleUserNew)
+
+	authenticatedRouter := NewRouter()
+	authenticatedRouter.GET("/images/new", HandleImageNew)
+
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		RenderTemplate(w, r, "index/home", nil)
-	})
-
 	mux.Handle(
 		"/assets/",
 		http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))),
 	)
 
-	http.ListenAndServe(":3000", mux)
+	middleware := Middleware{}
+	middleware.Add(unauthenticatedRouter)
+	middleware.Add(mux)
+	middleware.Add(http.HandlerFunc(AuthenticateRequest))
+	middleware.Add(authenticatedRouter)
+
+	http.ListenAndServe(":3000", middleware)
+}
+
+// Creates a new router
+func NewRouter() *httprouter.Router {
+	router := httprouter.New()
+
+	// Note this was
+//	router.NotFound = func(http.ResponseWriter, *http.Request) {}
+
+	router.NotFound = http.HandlerFunc(func(http.ResponseWriter, *http.Request) {})
+	return router
 }
--- template.go
+++ template.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"net/http"
+)
+
+var layoutFuncs = template.FuncMap{
+	"yield": func() (string, error) {
+		return "", fmt.Errorf("yield called inappropriately")
+	},
+}
+
+var layout = template.Must(
+	template.New("layout.html").Funcs(layoutFuncs).ParseFiles("templates/layout.html"),
 )
 
 var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
@@ -17,10 +28,21 @@ var errorTemplate = `
 </html>
 `
 
-func RenderTemplate(w http.ResponseWriter, request *http.Request, name string, data interface{}) {
-	err := templates.ExecuteTemplate(w, name, data)
+func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error) {
+			buf := bytes.NewBuffer(nil)
+			err := templates.ExecuteTemplate(buf, name, data)
+			return template.HTML(buf.String()), err
+		},
+	}
+
+	layoutClone, _ := layout.Clone()
+	layoutClone.Funcs(funcs)
+	err := layoutClone.Execute(w, data)
+
 	if err != nil {
-		w.WriteHeader(http.StatusInternalError)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, errorTemplate, name, err.Error())
 	}
 }
